3af3ae3c147dd64f822a627db766f7426ed3eb61
capac <capac@users.noreply.github.com>
2020-12-21 18:06:32 +0000

    Testing exiftool with subprocess

--- rename-photos.py
+++ rename-photos.py
@@ -2,8 +2,11 @@
 
 import os
 import glob
+import subprocess
 
-os.chdir(r'/Users/angelo/Pictures/Lightroom')
+os.chdir('/Users/angelo/Pictures/Lightroom/Dino')
 
-photos = glob.glob(r'**/*.[JjPpe?E?Gg]', recursive=True)
-print(photos)
+photos = glob.glob('**/*.*', recursive=True)
+print(len(photos))
+
+(subprocess.call(['exiftool', '-d', '%Y%m%d_%H%M%S%%-c.%%e', '-testname<${Date;}', '.']))
