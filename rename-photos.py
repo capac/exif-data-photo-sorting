2cb520c30f5b4a313a7f3a3d12a4229baad573f2
capac <capac@users.noreply.github.com>
2020-12-22 18:02:35 +0000

    Evaluating different date outputs

--- rename-photos.py
+++ rename-photos.py
@@ -4,16 +4,20 @@ import os
 import glob
 import subprocess
 
-os.chdir('/Users/angelo/Pictures/Lightroom/Dino')
+os.chdir('/Users/angelo/Pictures/Lightroom/Angelo\'s Pics')
 
 photos = glob.glob('**/*.*', recursive=True)
 print(len(photos))
-
+time_tags = ['DateTimeOriginal', 'CreateDate', 'DateCreated', 'Date']
+counter = 0
 for photo in photos:
     # subprocess.run(['exiftool', '-d', '%Y%m%d_%H%M%S%%-c.%%e', '-testname<${Date;}', photo])
-    date_time_all = subprocess.run('exiftool -s -f -time:all '+photo+' | awk \'{print $3 $4}\'',
-                                   shell=True, capture_output=True)
+    date_time_all = subprocess.run('exiftool -s -f -time:all '+photo, shell=True, capture_output=True, text=True)
+    # date_time_all = subprocess.run('exiftool -s -f -time:all '+photo+' | awk \'{print $3, $4}\'',
+    #                                shell=True, capture_output=True, text=True)
     print(str(date_time_all.stdout))
-    break
+    counter += 1
+    if counter % 3 == 0:
+        break
     # date_time = date_time_all.split(':')[1]
     # print(date_time_all)
