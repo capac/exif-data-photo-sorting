d1b04bfe6b110d1eaafd92f8549fc39759f3e780
capac <capac@users.noreply.github.com>
2020-12-22 17:26:52 +0000

    Saving stdout from ExifTool using subprocess

--- rename-photos.py
+++ rename-photos.py
@@ -9,4 +9,11 @@ os.chdir('/Users/angelo/Pictures/Lightroom/Dino')
 photos = glob.glob('**/*.*', recursive=True)
 print(len(photos))
 
-(subprocess.call(['exiftool', '-d', '%Y%m%d_%H%M%S%%-c.%%e', '-testname<${Date;}', '.']))
+for photo in photos:
+    # subprocess.run(['exiftool', '-d', '%Y%m%d_%H%M%S%%-c.%%e', '-testname<${Date;}', photo])
+    date_time_all = subprocess.run('exiftool -s -f -time:all '+photo+' | awk \'{print $3 $4}\'',
+                                   shell=True, capture_output=True)
+    print(str(date_time_all.stdout))
+    break
+    # date_time = date_time_all.split(':')[1]
+    # print(date_time_all)
