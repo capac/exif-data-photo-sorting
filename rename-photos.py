ea6efebb6fae2203b862bdeeb8c88e81dbd4088b
capac <capac@users.noreply.github.com>
2020-12-24 18:42:01 +0000

    First working prototype for name changing

--- rename-photos.py
+++ rename-photos.py
@@ -4,20 +4,27 @@ import os
 import glob
 import subprocess
 
-os.chdir('/Users/angelo/Pictures/Lightroom/Angelo\'s Pics')
+os.chdir('/Users/angelo/Pictures/Lightroom/Mark\'s Pics')
 
-photos = glob.glob('**/*.*', recursive=True)
-print(len(photos))
-time_tags = ['DateTimeOriginal', 'CreateDate', 'DateCreated', 'Date']
-counter = 0
+photos = sorted(glob.glob('**/*.*', recursive=True), key=os.path.getctime, reverse=True)
+print(f'Number of photos: {len(photos)}\n')
+datetime_tags = ['DateTimeOriginal', 'CreateDate', 'DateCreated',
+                 'Date', 'ModifyDate', 'FileModifyDate']
+
 for photo in photos:
-    # subprocess.run(['exiftool', '-d', '%Y%m%d_%H%M%S%%-c.%%e', '-testname<${Date;}', photo])
-    date_time_all = subprocess.run('exiftool -s -f -time:all '+photo, shell=True, capture_output=True, text=True)
-    # date_time_all = subprocess.run('exiftool -s -f -time:all '+photo+' | awk \'{print $3, $4}\'',
-    #                                shell=True, capture_output=True, text=True)
-    print(str(date_time_all.stdout))
-    counter += 1
-    if counter % 3 == 0:
-        break
-    # date_time = date_time_all.split(':')[1]
-    # print(date_time_all)
+    photo_name, photo_ext = os.path.splitext(photo)
+    for tag in datetime_tags:
+        photo_run_output = subprocess.run('exiftool -s -f -time:'+tag+' '+'"'+photo+'"',
+                                          shell=True, capture_output=True, text=True)
+        if photo_run_output.stdout.split(':')[1] != ' -\n':
+            photo_date_time = photo_run_output.stdout.split(': ')[1]
+            date_time = photo_date_time.split(' ')
+            date_time[1] = date_time[1][0:8]
+            new_dt_list = []
+            for dt in date_time:
+                new_dt_list.append('-'.join(dt.split(':')))
+            new_dt = '_'.join(new_dt_list)
+            print(f'Photo: {photo}, tag: {tag}, new name: {new_dt}{photo_ext.lower()}\n')
+            break
+    else:
+        print(photo.split('-')[0]+'_NO-CHANGE-IN-FILE-NAME\n')
