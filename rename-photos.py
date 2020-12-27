fb27ec9478978bcfffb97131c57925012c6421b0
capac <capac@users.noreply.github.com>
2020-12-27 21:46:34 +0000

    Added case if no date/time tag is present

--- rename-photos.py
+++ rename-photos.py
@@ -4,7 +4,7 @@ import os
 import glob
 import subprocess
 
-os.chdir('/Users/angelo/Pictures/Lightroom/Mark\'s Pics')
+os.chdir('/Users/angelo/Pictures/Lightroom/Angelo\'s Pics/')
 
 photos = sorted(glob.glob('**/*.*', recursive=True), key=os.path.getctime, reverse=True)
 print(f'Number of photos: {len(photos)}\n')
@@ -14,17 +14,15 @@ datetime_tags = ['DateTimeOriginal', 'CreateDate', 'DateCreated',
 for photo in photos:
     photo_name, photo_ext = os.path.splitext(photo)
     for tag in datetime_tags:
-        photo_run_output = subprocess.run('exiftool -s -f -time:'+tag+' '+'"'+photo+'"',
-                                          shell=True, capture_output=True, text=True)
-        if photo_run_output.stdout.split(':')[1] != ' -\n':
-            photo_date_time = photo_run_output.stdout.split(': ')[1]
-            date_time = photo_date_time.split(' ')
-            date_time[1] = date_time[1][0:8]
-            new_dt_list = []
-            for dt in date_time:
-                new_dt_list.append('-'.join(dt.split(':')))
-            new_dt = '_'.join(new_dt_list)
-            print(f'Photo: {photo}, tag: {tag}, new name: {new_dt}{photo_ext.lower()}\n')
+        photo_run_output = subprocess.run('exiftool -s -f \'-filename<'+tag+'\'\
+        -d %Y-%m-%d_%H-%M-%S%%-c.%%e '+'"'+photo+'"', shell=True, capture_output=True, text=True)
+        if photo_run_output.returncode == 0:
+            new_photo = f'{photo_name}{photo_ext.lower()}'
+            print(new_photo)
+            os.rename(photo, new_photo)
             break
     else:
-        print(photo.split('-')[0]+'_NO-CHANGE-IN-FILE-NAME\n')
+        new_photo = f'{photo_name[0:8]}{photo_ext.lower()}'
+        print(new_photo)
+        os.rename(photo, new_photo)
+        break
