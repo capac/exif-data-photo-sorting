ed35a8c4dea052d4654c8b55222fc2e0a2433907
capac <capac@users.noreply.github.com>
2020-12-28 15:42:58 +0000

    Added code to use lowercase for file extension

--- rename-photos.py
+++ rename-photos.py
@@ -26,3 +26,10 @@ for photo in photos:
         os.rename(photo, new_photo)
         print(photo_name)
         break
+
+new_photos = sorted(glob.glob('**/*.*', recursive=True))
+for new_photo in new_photos:
+    new_photo_name, new_photo_ext = os.path.splitext(new_photo)
+    new_photo_with_lower_case_ext = f'{new_photo_name}{new_photo_ext.lower()}'
+    os.rename(new_photo, new_photo_with_lower_case_ext)
+    print(new_photo_with_lower_case_ext)
