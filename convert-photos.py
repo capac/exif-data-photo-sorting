1e63818bd782cfb0f87da62d5d608b33fff14332
capac <capac@users.noreply.github.com>
2020-12-28 16:25:42 +0000

    Converts DNG to JPG using 92, which is default quality in Imagemagick

--- convert-photos.py
+++ convert-photos.py
@@ -4,15 +4,15 @@ import subprocess
 import os
 import glob
 
-os.chdir(r'/Users/angelo/Pictures/Lightroom/Allegra Scansetti/2011')
+os.chdir(r'/Users/angelo/Pictures/Lightroom/Allegra Scansetti/2012')
 photos = glob.glob('**/*.dng', recursive=True)
-
-quality = 96
+quality = str(92)
 
 for photo in photos:
-    base_name, ext_name = os.path.split(photo)
+    base_name, ext_name = os.path.splitext(photo)
+    print(f'Photo: {photo}')
     cmd = '/usr/local/bin/magick convert -quality '+quality+' "'+base_name+'.dng" "'+base_name+'.jpg"'
     conversion_output = subprocess.run(cmd, shell=True, text=True, capture_output=True)
     if conversion_output.returncode == 0:
         if conversion_output.stderr == '':
-            break
+            print(f'Photo {photo} successfully converted to JPG.')
