5d475bb77fab3e41072300c94d8df9f0ab5b384c
capac <capac@users.noreply.github.com>
2020-12-28 15:42:04 +0000

    Using ExifTool returncode and stderr output for file renaming

--- rename-photos.py
+++ rename-photos.py
@@ -4,7 +4,7 @@ import os
 import glob
 import subprocess
 
-os.chdir('/Users/angelo/Pictures/Lightroom/Angelo\'s Pics/')
+os.chdir(r'/Users/angelo/Pictures/Lightroom/Allegra Scansetti/2013')
 
 photos = sorted(glob.glob('**/*.*', recursive=True), key=os.path.getctime, reverse=True)
 print(f'Number of photos: {len(photos)}\n')
@@ -14,15 +14,15 @@ datetime_tags = ['DateTimeOriginal', 'CreateDate', 'DateCreated',
 for photo in photos:
     photo_name, photo_ext = os.path.splitext(photo)
     for tag in datetime_tags:
-        photo_run_output = subprocess.run('exiftool -s -f \'-filename<'+tag+'\'\
-        -d %Y-%m-%d_%H-%M-%S%%-c.%%e '+'"'+photo+'"', shell=True, capture_output=True, text=True)
+        string = 'exiftool -s -f \'-filename<'+tag+'\' -d %Y-%m-%d_%H-%M-%S%%-c.%%e '+'"'+photo+'"'
+        photo_run_output = subprocess.run(string, shell=True, capture_output=True, text=True)
+        print(photo_run_output)
         if photo_run_output.returncode == 0:
-            new_photo = f'{photo_name}{photo_ext.lower()}'
-            print(new_photo)
-            os.rename(photo, new_photo)
-            break
+            if photo_run_output.stderr == '':
+                print(photo_name)
+                break
     else:
         new_photo = f'{photo_name[0:8]}{photo_ext.lower()}'
-        print(new_photo)
         os.rename(photo, new_photo)
+        print(photo_name)
         break
