7603d040e7cf0586900164049c654cb823df12c9
capac <capac@users.noreply.github.com>
2020-12-29 18:51:48 +0000

    Modified file renaming in case renaming from tags fails

--- rename-photos.py
+++ rename-photos.py
@@ -5,10 +5,10 @@ import glob
 import subprocess
 import logging
 
-logging.basicConfig(filename='rename.log', level=logging.DEBUG,
+logging.basicConfig(filename='rename.log', level=logging.INFO,
                     format='%(asctime)s:%(levelname)s:%(message)s')
 
-os.chdir(r'/Users/angelo/Pictures/Lightroom/Allegra Scansetti/2013')
+os.chdir(r'/Users/angelo/Pictures/Lightroom/Allegra Scansetti/2014')
 
 photos = sorted(glob.glob('**/*.*', recursive=True), reverse=True)
 
@@ -19,21 +19,20 @@ datetime_tags = ['DateTimeOriginal', 'CreateDate', 'DateCreated',
                  'Date', 'ModifyDate', 'FileModifyDate']
 
 for photo in photos:
-    photo_name, photo_ext = os.path.splitext(photo)
     # go through datetime tags to rename file according to first available tag
     for tag in datetime_tags:
-        cmd = 'exiftool -s -F -f \'-filename<'+tag+'\' -d %Y-%m-%d_%H-%M-%S%%-c.%%e '+'\"'+photo+'\"'
+        cmd = 'exiftool -s -F -f \'-FileName<'+tag+'\' -d %Y-%m-%d_%H-%M-%S%%-c.%%e '+'\"'+photo+'\"'
         photo_run_output = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-        logging.debug(photo_run_output)
-        # use returncode and stderr to determine
-        # if file has been properly renamed
+        logging.info(photo_run_output)
+        # use returncode to determine if
+        # file has been properly renamed
         if photo_run_output.returncode == 0:
-            if photo_run_output.stderr == '':
-                print(photo_name)
-                break
+            print(photo)
+            break
     # if none of the tags are found, use
     # then first 8 characters to rename file
     else:
+        photo_name, photo_ext = os.path.splitext(photo)
         new_photo = f'{photo_name[0:8]}{photo_ext.lower()}'
         os.rename(photo, new_photo)
         print(photo_name)
