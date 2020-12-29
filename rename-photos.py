d5f311e07004b88986ad06ec3b0d86b2c69a6e21
capac <capac@users.noreply.github.com>
2020-12-29 20:41:38 +0000

    Removed 'os.rename()' section

--- rename-photos.py
+++ rename-photos.py
@@ -21,22 +21,23 @@ datetime_tags = ['DateTimeOriginal', 'CreateDate', 'DateCreated',
 for photo in photos:
     # go through datetime tags to rename file according to first available tag
     for tag in datetime_tags:
-        cmd = 'exiftool -s -F -f \'-FileName<'+tag+'\' -d %Y-%m-%d_%H-%M-%S%%-c.%%e '+'\"'+photo+'\"'
+        cmd = 'exiftool -s -F -v -f \'-FileName<'+tag+'\' -d %Y-%m-%d_%H-%M-%S%%-c.%%e '+'\"'+photo+'\"'
         photo_run_output = subprocess.run(cmd, shell=True, capture_output=True, text=True)
         logging.info(photo_run_output)
-        # use returncode to determine if
-        # file has been properly renamed
+        # use returncode and stderr to determine
+        # if file has been properly renamed
         if photo_run_output.returncode == 0:
-            print(photo)
-            break
+            if photo_run_output.stderr == '':
+                print(photo)
+                break
     # if none of the tags are found, use
     # then first 8 characters to rename file
-    else:
-        photo_name, photo_ext = os.path.splitext(photo)
-        new_photo = f'{photo_name[0:8]}{photo_ext.lower()}'
-        os.rename(photo, new_photo)
-        print(photo_name)
-        break
+    # else:
+    #     photo_name, photo_ext = os.path.splitext(photo)
+    #     new_photo = f'{photo_name[0:8]}{photo_ext.lower()}'
+    #     os.rename(photo, new_photo)
+    #     print(photo_name)
+    #     break
 
 # make sure file extension is lowercase, for some camera
 # photos are saved with upper case extensions.
@@ -45,4 +46,4 @@ for new_photo in new_photos:
     new_photo_name, new_photo_ext = os.path.splitext(new_photo)
     new_photo_with_lower_case_ext = f'{new_photo_name}{new_photo_ext.lower()}'
     os.rename(new_photo, new_photo_with_lower_case_ext)
-    print(new_photo_with_lower_case_ext)
+    print(f'Lower case extension: {new_photo_with_lower_case_ext}')
